23c03ba0e29a7148a41d09a99e2bed08eb2a23ec
Kevin Xu <kevinbxu@gmail.com>
2019-07-15 17:25:42 -0400

    Create GenerateHands.py

---
+++ GenerateHands.py
@@ -0,0 +1,28 @@
+import random
+
+handSize = 10
+numPerfect = 0
+arr = []
+
+for i in range(1000):
+    for j in range(handSize):
+        check = True
+        x = random.randint(1,52)
+        arr.append(x)
+        if(x > 4 * handSize):
+            check = False
+
+    for k in range(handSize):
+        isNumberThere = False
+        for l in arr:
+            if (l == k):
+                isNumberThere = True
+        if(isNumberThere == False):
+            check = False
+
+    if(check == True):
+       numPerfect += 1
+                
+        
+
+print (numPerfect / 1000.0)
