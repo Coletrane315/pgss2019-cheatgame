b8b5b24c7f7226a1a3ee0a4e67d9fca6407a6c93
larab3dc <52798954+larab3dc@users.noreply.github.com>
2019-07-16 19:54:08 -0400

    Update GenerateHands.py
    added input for variablility of handsize, Kevin, what do you want the x > 4 * handsize to prove?

--- GenerateHands.py
+++ GenerateHands.py
@@ -4,19 +4,19 @@ numPlayers = float(input("How many players are there playing?: "))
 handSize = round(52/numPlayers)
 
 numPerfect = 0
-arr = []
+hand = []
 
 for i in range(1000):
     for j in range(handSize):
         check = True
         x = random.randint(1,52)
-        arr.append(x)
+        hand.append(x)
         if(x > 4 * handSize):
             check = False
 
     for k in range(handSize):
         isNumberThere = False
-        for l in arr:
+        for l in hand:
             if (l == k):
                 isNumberThere = True
         if(isNumberThere == False):
