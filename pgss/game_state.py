64201d26c8173834a36939531932e7294cacc3ed
Taryn Silvernale <taryns@andrew.cmu.edu>
2019-07-18 16:39:47 -0400

    Update game_state.py
    began to establish variables for number of cards in each players hand and number of cards in the center

--- pgss/game_state.py
+++ pgss/game_state.py
@@ -5,3 +5,16 @@ class GameState:
     
     def __init__(self):
         # setup your variables here
+
+    def CardinOthersHand():
+        #P1 = you
+        self.NumCardsP1 = 
+        self.NumCardsP2 =
+        self.NumCardsP3 =
+        self.NumcardsP4 =
+        
+    def CardsinPile():
+        self.CardsinPile = (52-(NumCardsP1 + NumCardsP2 + NumCardsP3 + NumCardsP4)
+        
+
+                       
