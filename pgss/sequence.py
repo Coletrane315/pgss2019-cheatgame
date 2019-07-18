17155e853b7d23edd23c28d13c774b2269ca084e
cheatingthemichal <michalhajlasz@gmail.com>
2019-07-18 16:32:13 -0400

    I did the thing that the file is supposed to do

--- pgss/sequence.py
+++ pgss/sequence.py
@@ -1,5 +1,10 @@
 class SequenceCalculator:
 
     def calculateSequence(self,game_state):
-        # calculate the sequence here
-        pass
+        start = True
+        sequenceTemp = self.position #The person to the left of the dealer is player 1 and plays an Ace. If one is immidiately to the left of player 1, they are player 2.
+        while ((self.position != sequenceTemp) or (start == True)):
+            self.sequence.append(sequenceTemp)
+            sequenceTemp = (sequenceTemp + self.players -1)%13 + 1
+            start = False
+#change variable names as necessary
