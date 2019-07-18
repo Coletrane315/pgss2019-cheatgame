191e389a2b43cd9c252447ee8803819bdcf5b493
Taryn Silvernale <taryns@andrew.cmu.edu>
2019-07-18 17:12:53 -0400

    added card types

--- pgss/game_state.py
+++ pgss/game_state.py
@@ -6,15 +6,36 @@ class GameState:
     def __init__(self):
         # setup your variables here
 
-    def cards_in_others_hand():
-        #P1 = you
-        self.num_cards_p1 = 
-        self.num_cards_p2 =
-        self.num_cards_p3 =
-        self.num_cards_p4 =
+    def num_cards_in_others_hand(self):
+        self.num_cards_self = 0 
+        self.num_cards_p2 = 0
+        self.num_cards_p3 = 0
+        self.num_cards_p4 = 0
         
-    def cards_in_pile():
-        self.cards_in_pile = (52-(num_cards_p1 + num_cards_p2 + num_cards_p3 + num_cards_p4)
+    def cards_in_pile(self):
+        self.cards_in_pile = (52-(num_cards_p1 + num_cards_p2 + num_cards_p3 + num_cards_p4))
+
+    def card_types_in_own_hand (self):
+        self.ace = 0
+        self.two = 0
+        self.three = 0
+        self.four = 0
+        self.five = 0
+        self.six = 0
+        self.seven = 0
+        self.seven = 0
+        self.eight = 0
+        self.nine = 0
+        self.ten = 0
+        self.jack = 0
+        self.queen = 0
+        self.king = 0
+        
+       
+        
+    
+
+        
         
 
                        
