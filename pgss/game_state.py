5781f65944a86be23689ced872b375922cb2e664
rapinchie <52933397+rapinchie@users.noreply.github.com>
2019-07-22 16:22:14 -0400

    Dictionary of player hands
    Creates a dictionary containing lists of each player's hand

--- pgss/game_state.py
+++ pgss/game_state.py
@@ -19,3 +19,21 @@ class Players:
         num_cards_in_hands.update({name_player : 0})
         loop_player += 1
 
+    #this is the number of players!
+    number_of_players = 0
+
+    #define hands dictionary as empty for the time being
+    hands = {}
+    for x in range(number_of_players):
+        #where 'n' is the number of cards in the nth player's hand
+        n = 0 
+        #lst is a dummy variable that will store the cards in the nth player's hand during the loop
+        lst = []
+        for i in range(n): 
+            ele = 0 
+            #this loop will append the player's hand with card 'ele' 
+            lst.append(ele)
+        #Stores lst as the nth player's hand
+        hands[x] = lst
+
+    #Now you have a dictionary containing lists of all the player's hands!
