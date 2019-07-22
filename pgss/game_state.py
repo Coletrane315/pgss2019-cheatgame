060e96a62b510f3a45d3928c015393a08750e0f2
larab3dc <52798954+larab3dc@users.noreply.github.com>
2019-07-22 16:17:32 -0400

    Just redid it all

--- pgss/game_state.py
+++ pgss/game_state.py
@@ -5,82 +5,17 @@ class GameState:
     
     def __init__(self):
         # setup your variables here
-        self.num_cards_self = 0 
-        self.num_cards_p2 = 0
-        self.num_cards_p3 = 0
-        self.num_cards_p4 = 0
-        self.cycles_until_win = 0
-        self.cycles_until_p2_win = 0
-        self.cycles_until_p3_win = 0
-        self.cycles_until_p4_win = 0
-        self.sequence_self = []
-        self.sequence_p2 = []
-        self.sequence_p3 = []
-        self.sequence_p4 = []
-        self.cards_in_pile = (52-(num_cards_self + num_cards_p2 + num_cards_p3 + num_cards_p4))
         
-    def card_types_in_own_hand (self):
-        self.ace = 0
-        self.two = 0
-        self.three = 0
-        self.four = 0
-        self.five = 0
-        self.six = 0
-        self.seven = 0
-        self.seven = 0
-        self.eight = 0
-        self.nine = 0
-        self.ten = 0
-        self.jack = 0
-        self.queen = 0
-        self.king = 0
+        self.cycles_until_win = 
+        self.sequence_self = []
 
-    def card_types_in_p2_hand:
-        self.ace = 0
-        self.two = 0
-        self.three = 0
-        self.four = 0
-        self.five = 0
-        self.six = 0
-        self.seven = 0
-        self.seven = 0
-        self.eight = 0
-        self.nine = 0
-        self.ten = 0
-        self.jack = 0
-        self.queen = 0
-        self.king = 0
+        
+class Players:
+    #position 0 is our position
+    num_cards_in_hands = {}
+    loop_player = 0
+    for x in range(num_players):
+        name_player = "num_cards_p" + str(loop_player)
+        num_cards_in_hands.update({name_player : 0})
+        loop_player += 1
 
-    def card_types_in_p3_hand:
-        self.ace = 0
-        self.two = 0
-        self.three = 0
-        self.four = 0
-        self.five = 0
-        self.six = 0
-        self.seven = 0
-        self.seven = 0
-        self.eight = 0
-        self.nine = 0
-        self.ten = 0
-        self.jack = 0
-        self.queen = 0
-        self.king = 0
-
-    def card_types_in_p4_hand:
-        self.ace = 0
-        self.two = 0
-        self.three = 0
-        self.four = 0
-        self.five = 0
-        self.six = 0
-        self.seven = 0
-        self.seven = 0
-        self.eight = 0
-        self.nine = 0
-        self.ten = 0
-        self.jack = 0
-        self.queen = 0
-        self.king = 0
-
-                       
