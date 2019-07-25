f5f621f506e8a1536ab7683e3c62098cafa5e50c
larab3dc <52798954+larab3dc@users.noreply.github.com>
2019-07-25 14:48:40 -0400

    added more variables and got rid of "of" in variable name

--- pgss/game_state.py
+++ pgss/game_state.py
@@ -8,19 +8,22 @@ class GameState:
         
         self.cycles_until_win = []
         self.sequence_self = []
-        self.num_cards_in_hands = num_cards_in_hands
+        self.num_cards_hands = num_cards_in_hands
         self.cards_of_player = cards_of_player
+        self.num_cards_center = 52 - sum(num_cards_hands.values())
+        self.known_cards_center = []
+        self.num_played_cards = 0
 
 num_players = 5
 
-num_cards_in_hands = {}
+num_cards_hands = {}
 cards_of_player = {}
 loop_player = 0
 for x in range(num_players):
     name_num_cards = "num_cards_p" + str(loop_player)
     name_player = "player_hands" + str(loop_player)
     cards = []
-    num_cards_in_hands.update({name_num_cards : 0})
+    num_cards_hands.update({name_num_cards : 0})
     cards_of_player.update({name_player : cards_of_player})
     loop_player += 1
 
@@ -28,5 +31,5 @@ for x in range(num_players):
 
 test = GameState()
 
-print(test.num_cards_in_hands)
+print(test.num_cards_center)
 print(test.cards_of_player)
