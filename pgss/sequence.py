3fafb849a174997ca25dee7bc9fff2af9f131c0d
cheatingthemichal <michalhajlasz@gmail.com>
2019-07-25 15:02:46 -0400

    I didn't edit the readme file, why do I have to commit it? Why am I alive? Imma miss some of y'all when this program is over, damn it's really coming to an end. It was fun being here. Anyways, how's life. I want to add some song lyrics here but I'm not sure what song to do. Lmk if you have some ideas.

--- pgss/sequence.py
+++ pgss/sequence.py
@@ -1,6 +1,6 @@
 class SequenceCalculator:
 
-    def calculateSequence(self,game_state):
+    def calculate_sequence(self,game_state):
         start = True
         sequenceTemp = self.position #The person to the left of the dealer is player 1 and plays an Ace. If one is immidiately to the left of player 1, they are player 2.
         while ((self.position != sequenceTemp) or (start == True)):
