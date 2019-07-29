41e5588b564774c46ffef51f679a8dc1b633cca6
20fwang <47217770+20fwang@users.noreply.github.com>
2019-07-29 16:33:27 -0400

    Update bluff.py

--- pgss/bluff.py
+++ pgss/bluff.py
@@ -6,12 +6,12 @@ class BluffCalculator:
         def prob_calculator(self, card_turn, game_state):
                 #A is the size of the bot's hand. B, C, and D are the hand sizes of players 2,3 and 4 respectively. n is the number of a
                 #certain card the bot doesn't have. If the turn is 2's and the bot has 1 2, then n=3.
-                A = game_state.__bot.__num_cards
+                A = game_state._bot._num_cards
                 opp_hands=[]
-                for player in game_state.__players:
-                        opp_hands.append(player.__num_cards)
+                for player in game_state._players:
+                        opp_hands.append(player._num_cards)
                 opp_hands.remove(A)
-                cards = game_state.__bot.num_each_cards
+                cards = game_state._bot.num_each_cards
                 chance_numerator = 0
                 n = 4 - cards[card_turn - 1]
                 for hands in opp_hands:
@@ -23,7 +23,7 @@ class BluffCalculator:
         #precondition: we have x number of cards and need to find out if we should lie or not 
         #postcondition: returns the card that we should lie with (0 means we shouldn't lie)
         def should_bluff(self,game_state, card_turn):
-            cards = game_state.__bot.num_each_cards
+            cards = game_state._bot._num_each_card
             num = cards[card_turn - 1]
             if num == 1:
                 return should_bluff_1_card(self, game_state) #kicks it to function to calculate when we have 1 card
@@ -57,5 +57,5 @@ class BluffCalculator:
             
         #returns what card we should lie with
         def pick_card_to_lie_with(self, game_state):
-            x = game_state.__bot.get_last_card_in_seq()
+            x = game_state._bot.get_last_card_in_seq()
             return x 
